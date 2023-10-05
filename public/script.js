96560fdcd95eaa0a28b3bb8136f32dcc768d70d9
StarshinePony <128289655+StarshinePony@users.noreply.github.com>
2023-10-05 19:39:47 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -108,6 +108,7 @@ fetch("/initialize")
 	.then(res => {
 		loggedIn = res.loggedIn;
 		banned = res.banned;
+		mod = res.mod;
 
 		if (isIOS()) // fix for iOS blurring the canvas for some odd reason... 
 		{
