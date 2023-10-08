ab84843c9aaa3eef6838ceb0b38013ffa3b60c27
StarshinePony <128289655+StarshinePony@users.noreply.github.com>
2023-10-08 03:31:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -163,12 +163,12 @@ app.get("/credits", (req, res) => {
 		  <div class="buttons">
 			  <a href="https://github.com/Manechat/place.manechat.net">GitHub Repository from Merc</a>
 			  <a href="https://github.com/StarshinePony/mareplace">Github Repository of this instance</a>
-			  <a href="/">Go back to the main page</a>
 		  </div>
 		  <p>Wanna use a template overlay? Download the Minimap here! [FYI] > You need the tampermonkey extension for this to work!</p>
 		  <div class="buttons">
 			  <a href="https://www.tampermonkey.net/">Tampermonkey extention</a>
 			  <a href="https://github.com/StarshinePony/2023-minimap/raw/main/minimap.user.js">Download Script</a>
+			  <a href="/">Go back to the main page</a>
 		  </div>
 	  </div>
   </body>
