db824dc064249f1507f32c695c11bfe775716d2d
StarshinePony <128289655+StarshinePony@users.noreply.github.com>
2023-10-15 15:23:39 +0200

    this is rlly dumb don't ask me i haven't done this at all......

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ async function userInfo(req, res, next) {
  * ===============================
 */
 
-const canvas = new Canvas().initialize({ sizeX: 500, sizeY: 500, colors: ["#6d001a", "#be0039", "#ff4500", "#ffa800", "#ffd635", "#fff8b8", "#00a368", "#00cc78", "#7eed56", "#00756f", "#009eaa", "#00ccc0", "#2450a4", "#3690ea", "#51e9f4", "#493ac1", "#6a5cff", "#94b3ff", "#811e9f", "#b44ac0", "#e4abff", "#de107f", "#ff3881", "#ff99aa", "#6d482f", "#9c6926", "#ffb470", "#000000", "#515252", "#898d90", "#d4d7d9", "#ffffff"] });
+const canvas = new Canvas().initialize({ sizeX: 500, sizeY: 500, colors: ["#6d001a", "#be0039", "#ff4500", "#ffa800", "#ffd635", "#fff8b8", "#00a368", "#00cc78", "#7eed56", "#00756f", "#009eaa", "#00ccc0", "#2450a4", "#3690ea", "#51e9f4", "#493ac1", "#6a5cff", "#94b3ff", "#293873", "#811e9f", "#b44ac0", "#e4abff", "#de107f", "#ff3881", "#ff99aa", "#6d482f", "#9c6926", "#ffb470", "#000000", "#d9e650", "#515252", "#898d90", "#d4d7d9", "#ffffff"] });
 const io = new Canvas.IO(canvas, "./canvas/current.hst").read();
 
 // day 2 colors
@@ -613,7 +613,7 @@ app.setUpSockets = () => {
 			});
 		});
 	} catch (error) {
-		console.log("Error while setting up websocket:",error);
+		console.log("Error while setting up websocket:", error);
 	}
 };
 app.setUpSockets();
